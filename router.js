ec42ea8e600966adeb65c9f38b7839b9f69db0a5
atroncoso2 <atroncoso2@uc.cl>
2021-01-24 20:39:24 -0300

    add to list(total): redone, simple add list working clean

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router');
 const mongoose = require('mongoose');
+const mongodb = require('mongodb');
 
 const UserTest = require('./db/UserTest');
 const List = require('./db/List');
@@ -8,107 +9,134 @@ const router = new Router();
 
 router
   .get('/', async (ctx, next) => {
-    await ctx.render('home', {});
+    console.log("/[GET]: ctx.request.body")
+    console.log(ctx.request.body)
+    await ctx.render('home', {})
   })
-  .get('/index', async (ctx, next) => {
-    await ctx.render('index', {
-      body: 'this is body'
-    });
-  })
-  .get('/form', async (ctx, next) => {
-    await ctx.render('form', {});
-  })
-  .post('/form', async (ctx, next) => {
-    let post_info = ctx.request.body;
-    console.log(post_info);
-    
-    if (!post_info.name || !post_info.age || !post_info.nationality) {
-        ctx.body = {message: 'wrong info'}
-    } else {
-        ctx.body = ctx.request.body;
-        let newUser = new UserTest({
-            name: post_info.name,
-            age: post_info.age,
-            nationality: post_info.nationality,
-            items: ['item1', 'item2', 'item3']
-        });
-        newUser.save((err, res) => {
-            if (err) {
-                ctx.body = {message: 'error'}
-            } else {
-                ctx.body = {
-                    message: 'error',
-                    content: post_info
-                }
-            }
-        });
-    }
-    await ctx.render('form', {});
-  })
-  .get('/listTest', async (ctx, next) => {
-    let response = null;
-    await UserTest.find((err, res) => {
-        if (err) {
-            console.log(err);
-        }
-        response = res;
-    });
-    console.log(response);
-    await ctx.render('listTest', {
-        users: response ? response:[],
-    })
+  .get('/newList', async (ctx, next) => {
+    console.log("newList[GET]: ctx.request.body")
+    console.log(ctx.request.body)
+    await ctx.render('newList', {})
   })
   .post('/newList', async (ctx, next) => {
-    let post_info = ctx.request.body;
-    console.log(post_info);
-    let listId = 0;
-    
-    if (!post_info.name) {
-        ctx.body = {message: 'wrong info'}
-        await ctx.render('home', {});
-    } else {
-      try {
-        let newList = new List({
-          code: 'code',
-          name: 'name',
-          users: [],
-          items: []
-        });
-        let saveList = await newList.save();
-        listId = saveList._id;
-      } catch(err) {
-        console.log(err);
-      }
-    }
-    console.log('listId');
-    console.log(listId);
-    ctx.redirect(`/list/${listId}`);
-  })
-  .get('/list/:listId', async (ctx, next) => {
-    console.log(ctx.params);
-    let response = null;
-    await List.find(mongoose.Types.ObjectId(ctx.params.listId), (err, res) => {
+    console.log("newList[POST]: ctx.request.body")
+    console.log(ctx.request.body)
+    let user = ctx.request.body.user
+    let newList = new List({
+        users: [user],
+        items: [
+          {
+            name: 'milk',
+            quantity: 3,
+            checked: false,
+          },
+        ],
+    });
+    newList.save((err, res) => {
       if (err) {
-          console.log(err);
+        console.log(err)
       }
-      response = res;
     })
-    console.log('response');
-    console.log(response);
-    if (response) {
-      response = response[0];
-      await ctx.render('list', {
-          listId: response._id,
-          name: response.name,
-          items: response.items,
-      })
-    } else {
-      await ctx.render('list', {
-          listId: null,
-          name: null,
-          items: null,
-      })
+    console.log("newList: ")
+    console.log(newList)
+    console.log(newList._id)
+    await ctx.redirect(`/list/${newList._id}`)
+  })
+  .post('/getList', async (ctx, next) => {
+    console.log("getList[POST]: ctx.request.body")
+    console.log(ctx.request.body)
+    let _id = ctx.request.body._id
+    let user = ctx.request.body.user
+    if (!mongodb.ObjectID.isValid(_id)) {
+      await ctx.redirect('/', {})
+      ctx.get()
+      return
     }
+    query = {
+      _id: mongodb.ObjectID(_id)
+    }
+    let exists = false
+    await List.findOne(query, (err, res) => {
+      if (res) {
+        exists = true
+      }
+    })
+    console.log(`exists: ${exists}`)
+    if (exists) {
+      await ctx.redirect(`/list/${_id}`)
+      return
+    }
+    await ctx.redirect('/', {})
+  })
+  .get('/list/:_id', async (ctx, next) => {
+    console.log("list/:_id[GET]: ctx.request.body")
+    console.log(ctx.request.body)
+    console.log("list/:_id[GET]: ctx.params")
+    console.log(ctx.params)
+    let _id = ctx.params._id
+    console.log(`_id: ${_id}`)
+    
+    if (!mongodb.ObjectID.isValid(_id)) {
+      await ctx.redirect('/', {})
+      return
+    }
+    query = {
+      _id: mongodb.ObjectID(_id)
+    }
+    let list;
+    await List.findOne(query, (err, res) => {
+      if (res) {
+        list = res;
+      }
+    })
+
+    console.log('list...')
+    console.log(list)
+
+    await ctx.render('list', {
+      items: [...list.items],
+      users: [...list.users],
+      _id: list._id,
+    })
+  })
+  .post('/addToList', async (ctx, next) => {
+    console.log("addToList[POST]: ctx.request.body")
+    console.log(ctx.request.body)
+    let _id = ctx.request.body._id
+    console.log(`_id: ${_id}`)
+    item = {
+      name: ctx.request.body.name,
+      quantity: ctx.request.body.quantity,
+      checked: false,
+    }
+    
+    if (!mongodb.ObjectID.isValid(_id)) {
+      await ctx.redirect('/', {})
+      return
+    }
+    query = {
+      _id: mongodb.ObjectID(_id)
+    }
+    let list
+    await List.findOne(query, (err, res) => {
+      if (res) {
+        list = res;
+      }
+    })
+    
+    console.log('prev list')
+    console.log(list)
+
+    list.items = [
+      item,
+      ...list.items,
+    ]
+    console.log('new list')
+    console.log(list)
+
+    await List.findOneAndUpdate(query, list, (err, res) => {})
+
+    await ctx.redirect(`/list/${_id}`)
   })
 
   module.exports = router;
